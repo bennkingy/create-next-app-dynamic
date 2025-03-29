d9e8ab33401fd0dd7113e9df79578e5231323f6d
Benn Dev <bennkingy@gmail.com>
2025-03-29 21:17:22 +0000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
 	webpack: (config) => {
 		config.externals.push('pino-pretty', 'lokijs', 'encoding')
 		return config
 	},
 	images: {
-    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'ui-avatars.com', 'img.reservoir.tools'],
+		domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'ui-avatars.com', 'img.reservoir.tools'],
 	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	}
 }
 
 module.exports = nextConfig
